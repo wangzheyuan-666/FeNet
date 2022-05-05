189f73401c0c9e4473cc54be3a93c39b933e7851
wangzheyuan-666 <66549413+wangzheyuan-666@users.noreply.github.com>
2022-05-05 16:43:43 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,118 @@
+import numpy as np
+import os,math,cv2
+import torch
+from collections import OrderedDict
+
+def shave(im, border):
+    border = [border, border]
+    im = im[border[0]:-border[0], border[1]:-border[1], ...]
+    return im
+
+
+def modcrop(im, modulo):
+    sz = im.shape
+    h = np.int32(sz[0] / modulo) * modulo
+    w = np.int32(sz[1] / modulo) * modulo
+    ims = im[0:h, 0:w, ...]
+    return ims
+
+
+def get_list(path, ext):
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith(ext)]
+
+
+def convert_shape(img):
+    img = np.transpose((img * 255.0).round(), (1, 2, 0))
+    img = np.uint8(np.clip(img, 0, 255))
+    return img
+
+
+def quantize(img):
+    return img.clip(0, 255)
+    # return img.clip(0, 255).round().astype(np.uint8)
+
+
+def tensor2np(tensor, out_type=np.uint8, min_max=(0, 1)):
+    tensor = tensor.float().cpu().clamp_(*min_max)
+    tensor = (tensor - min_max[0]) / (min_max[1] - min_max[0])  # to range [0, 1]
+    img_np = tensor.numpy()
+    img_np = np.transpose(img_np, (1, 2, 0))
+    if out_type == np.uint8:
+        img_np = (img_np * 255.0).round()
+
+    return img_np.astype(out_type)
+
+def convert2np(tensor):
+    return tensor.cpu().mul(255).clamp(0, 255).byte().squeeze().permute(1, 2, 0).numpy()
+
+
+def adjust_learning_rate(optimizer, epoch, step_size, lr_init, gamma):
+    factor = epoch // step_size
+    lr = lr_init * (gamma ** factor)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+def load_state_dict(path):
+
+    state_dict = torch.load(path)
+    new_state_dcit = OrderedDict()
+    for k, v in state_dict.items():
+        if 'module' in k:
+            name = k[7:]
+        else:
+            name = k
+        new_state_dcit[name] = v
+    return new_state_dcit
+
+def psnr(img1, img2):
+    # img1 and img2 have range [0, 255]
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    mse = np.mean((img1 - img2)**2)
+    if mse == 0:
+        return float('inf')
+    return 20 * math.log10(255.0 / math.sqrt(mse))
+def ssim(img1, img2):
+    C1 = (0.01 * 255)**2
+    C2 = (0.03 * 255)**2
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1**2
+    mu2_sq = mu2**2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+def compute_psnr(im1, im2):
+    p = psnr(im1, im2)
+    return p
+def compute_ssim(img1, img2):
+    '''calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    '''
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    if img1.ndim == 2:
+        return ssim(img1, img2)
+    elif img1.ndim == 3:
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim(img1, img2))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
